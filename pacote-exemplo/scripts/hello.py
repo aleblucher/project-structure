3236252865a7e32643d79a7fd6ec9aec255b52f1
aleblucher <alessandrablucher@hotmail.com>
2019-09-12 09:53:55 -0300

    Versao 2

--- pacote-exemplo/scripts/hello.py
+++ pacote-exemplo/scripts/hello.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env/ pyhton3
 import my_hello
 
 my_hello.world()
--- pacote-exemplo/setup.py
+++ pacote-exemplo/setup.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env/ pyhton3
+from setuptools import setup
 
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
+setup(
     name="aleblucher",
     version="0.0.1",
     author="Alessandra Blucher",
     author_email="emaildaalessandra@example.com",
     description="Descricao",
-    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/aleblucher/project-structure",
     scripts = ['scripts/hello.py'],
-    packages=setuptools.find_packages(),
     install_requires = ['decorator==4.4.0'],
     classifiers=[
         "Programming Language :: Python :: 3",
